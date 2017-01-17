5a9a551097fd780f13086c2042175ca8c1cfa0dd
Nsajdr <hey4ng@163.com>
2017-01-17 16:02:31 +0800

    OpenGL ES 2.0 show landform

---
+++ app/src/main/java/com/icegps/landformapplication/LandformActivity.java
@@ -0,0 +1,13 @@
+package com.icegps.landformapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LandformActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landform);
+    }
+}
